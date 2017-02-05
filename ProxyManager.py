15ab26b172a634a9b6f708138471c9f2e8275b0e
bergarces <nitrosonline@gmail.com>
2017-02-05 16:30:25 +0000

    File deletion ignores .gitkeep file

--- ProxyManager.py
+++ ProxyManager.py
@@ -1,3 +1,4 @@
+import time
 import re
 import os
 from os import listdir
@@ -7,20 +8,21 @@ from Proxy import Proxy
 
 
 while True:
-    fileList = [f for f in listdir('ProxyFiles') if isfile(join('ProxyFiles', f))]
+    fileList = [f for f in listdir('ProxyFiles') if isfile(join('ProxyFiles', f)) and f != '.gitkeep']
     for file in fileList:
-        # lines = [line.strip() for line in open('ProxyFiles/ProxyList')]
-        # for line in lines:
-        #     if re.match('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$', line) is not None:
-        #         proxyParts = line.split(":")
-        #         print(line + ' - ', end='', flush=True)
-        #
-        #         px = Proxy(proxyParts[0], int(proxyParts[1]))
-        #         if px.updateProxy():
-        #             print('WORKING', flush=True)
-        #         else:
-        #             print(flush=True)
-        #     else:
-        #         print("Odd line: " + line, flush=True)
+        lines = [line.strip() for line in open('ProxyFiles/ProxyList')]
+        for line in lines:
+            if re.match('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$', line) is not None:
+                proxyParts = line.split(":")
+                print(line + ' - ', end='', flush=True)
 
-        os.remove(file)
+                px = Proxy(proxyParts[0], int(proxyParts[1]))
+                if px.updateProxy():
+                    print('WORKING', flush=True)
+                else:
+                    print(flush=True)
+            else:
+                print("Odd line: " + line, flush=True)
+
+        os.remove(join('ProxyFiles', file))
+    time.sleep(60)
